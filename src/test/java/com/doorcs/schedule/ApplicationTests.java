13df7466534017a7ee6bd7a8a65f1b5e543a34a0
doorcs <castledoor@pusan.ac.kr>
2025-05-23 16:14:24 +0900

    Initial commit

---
+++ src/test/java/com/doorcs/schedule/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.doorcs.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
