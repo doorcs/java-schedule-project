9a88aadd98031f7e50f82df14b41621321aac61c
doorcs <castledoor@pusan.ac.kr>
2025-05-24 16:50:09 +0900

    feat: 예외 핸들러 추가 구현
    URL 오류, JDBC 오류 핸들러 구현

--- src/main/java/com/doorcs/schedule/controller/GlobalExceptionHandler.java
+++ src/main/java/com/doorcs/schedule/controller/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.doorcs.schedule.controller;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.doorcs.schedule.exception.BadRequestException;
 import com.doorcs.schedule.exception.NotFoundException;
@@ -13,6 +16,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ProblemDetail handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
+        log.warn("[Empty Result]", e);
+
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ProblemDetail handleNoResourceFoundException(NoResourceFoundException e) {
+        log.warn("[No Resource]", e);
+
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다.");
+    }
 
     @ExceptionHandler(BadRequestException.class)
     public ProblemDetail handleBadRequestException(BadRequestException e) {
@@ -26,6 +43,13 @@ public class GlobalExceptionHandler {
         log.warn("[Not Found]", e);
 
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ProblemDetail handleDataAccessException(DataAccessException e) {
+        log.error("[JDBC Error]", e);
+
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 오류가 발생했습니다.");
     }
 
     @ExceptionHandler(Exception.class)
